8b7d56de531129d503b5d96987eeb2644395e8f4
devSayca <jasonferone06@gmail.com>
2026-02-03 20:59:38 +0100

    Starting to implement PushSwap bonus

---
+++ bonus_checker.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bonus_checker.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jferone <jferone@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/03 18:36:33 by jferone           #+#    #+#             */
+/*   Updated: 2026/02/03 20:59:15 by jferone          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "push_swap.h"
+
+// Quick read: Reads a lign of STDIN (without a complete GNL for efficiency)
+static char	*read_instructions(void)
+{
+	
+}
+
+// Quick read: Exec the instruction in GCB
+static void	apply_instruction(t_gcb *gcb, char *line)
+{
+	
+}
+
+// Quick read: There is the main of BONUS MODE
+int	main(int argc, char **argv)
+{
+	
+} 
